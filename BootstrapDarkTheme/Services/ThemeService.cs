b6a7289359185ff2863915e5a40b5deac705d910
David Gallivan <33528347+CodingWithDavid@users.noreply.github.com>
2024-03-28 16:08:49 -0700

    Initial File Load

---
+++ BootstrapDarkTheme/Services/ThemeService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.JSInterop;
+
+namespace BootstrapDarkTheme
+{
+    public class ThemeService
+    {
+        private readonly IJSRuntime? js;
+        private bool isDarkTheme = false;
+
+        public bool IsDarkTheme 
+        { 
+            get => isDarkTheme;
+            set {
+                isDarkTheme = value;
+
+                // Set the theme in the browser
+                js.InvokeVoidAsync("setDarkTheme", value);
+
+                // Notify the app that the theme has changed
+                OnChange?.Invoke();
+            }
+        }
+
+        public ThemeService(IJSRuntime js)
+        {
+            this.js = js;
+        }
+
+        public async Task<bool> IsThemeDark() 
+        {
+            // Check if the browser is in dark mode
+            var result = await js.InvokeAsync<bool>("isBrowerDarkTheme");
+            return result;
+        }
+
+        public event Action? OnChange;
+
+        public async Task ListenForThemeChanged()
+        {
+            // Listen for theme changes in the browser
+            var ThemeObject = DotNetObjectReference.Create(this);
+            await js.InvokeVoidAsync("addListerForThemeChanged", ThemeObject);
+        }
+
+        [JSInvokable]
+        public async Task SetDarkTheme(bool isDark) => IsDarkTheme = isDark;
+    }
+}
